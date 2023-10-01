d546e7176555ce69d71376805966ed2ee8bd6dab
Gustas11 <vilimas.gustas@gmail.com>
2023-10-01 23:27:28 +0100

    worked on js for html tags + fixed some stuff

--- js/script.js
+++ js/script.js
@@ -10,6 +10,42 @@ function showSecretDiv1() {
     i++;
 }
 
+const gtlt = document.getElementsByClassName('code-symbols');
+const slashlt = document.getElementsByClassName('code-symbols-end');
+
+for(let i = 0; i < gtlt.length; i++) {
+    if(i % 2 == 0) {
+        gtlt[i].innerHTML += "&lt";
+        if (i == 0) {
+            gtlt[0].innerHTML += "!";
+        }
+    }
+    if(i % 2 != 0) {
+        gtlt[i].innerHTML = "&gt";
+    }
+}
+
+for(let i = 0; i < slashlt.length; i++) {
+    if(i % 2 == 0) {
+        slashlt[i].innerHTML = "&lt/";
+    } else {
+        slashlt[i].innerHTML = "&gt";
+    }
+}
+
 function swap(id, cls) {
     document.getElementById(id).classList.toggle(cls);
 }
+
+function write(id, text, k, sp) {
+    if (k < text.length) {
+      document.getElementById(id).innerHTML += text.charAt(k);
+      k++;
+      setTimeout(write, sp, id, text, k, sp);
+    }
+  }
+
+  
+  setTimeout(
+    write, 1300, "terminal-text", "PS C:\\Users\\vilim\\Documents\\githubprojects\\code-email-page-2>hi", 0, 20
+  )
